1b8af299624f8cc7ecc07308f04b577d0f419018
DiptaDev <111142705+DiptaDev@users.noreply.github.com>
2022-09-02 09:15:42 +0800

    Add files via upload

---
+++ allow.php
@@ -0,0 +1,14 @@
+<!-- THE HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Direct to datacenter</title>
+</head>
+<body>
+    <center> <h1>Click this button 🔽</h1> </center>
+    <center> <a href="./datacenter.php">Click here to direct to datacenter</a> </center>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,43 @@
+<!-- ip blocker system -->
+<?php
+
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
+$ip =get_client_ip();
+$ips = array();
+
+$pange = file("ip_block.txt");
+
+foreach($pange as $line)
+{
+
+    array_push($ips, $line);
+}
+
+if (in_array($ip, $ips))
+{
+  include_once('deny.html');
+}
+
+else
+{
+    include_once('allow.php');
+}
+?>
